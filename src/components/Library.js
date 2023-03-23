c6b3d016dc0dada6b4d34505b20e5d451c8c59b4
Ömer Selçuk Yatağan <oselcukyatagan@gmail.com>
2023-03-23 20:15:49 +0300

    LET HIM COOK

--- src/components/Library.js
+++ src/components/Library.js
@@ -23,3 +23,4 @@ export default function Library(){
         </div>
     )
 }
+
--- src/data.js
+++ src/data.js
@@ -22,5 +22,17 @@ export default [
         title: "The Alchemist",
         author: "Paulo Coelho",
         cover: "https://bloximages.newyork1.vip.townnews.com/dailynebraskan.com/content/tncms/assets/v3/editorial/d/54/d54908ec-8515-11e6-80b0-4337512dd3a6/57eb14a461906.image.jpg"
+    },
+    {
+        id: 5,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        cover: "https://bloximages.newyork1.vip.townnews.com/dailynebraskan.com/content/tncms/assets/v3/editorial/d/54/d54908ec-8515-11e6-80b0-4337512dd3a6/57eb14a461906.image.jpg"
+    },
+    {
+        id: 6,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        cover: "https://bloximages.newyork1.vip.townnews.com/dailynebraskan.com/content/tncms/assets/v3/editorial/d/54/d54908ec-8515-11e6-80b0-4337512dd3a6/57eb14a461906.image.jpg"
     }
 ]
