853ff8ce9426b7a40015b365bfb00a331ceb02cb
oselcukyatagan <115888665+oselcukyatagan@users.noreply.github.com>
2023-03-27 14:40:07 +0300

    probably going to break after this

--- src/pages/Opening.js
+++ src/pages/Opening.js
@@ -1,18 +1,35 @@
-import React from "react"
-import {Link} from "react-router-dom";
-import {FaBookOpen} from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import "../opening.css"
+import {Link} from "react-router-dom"
 
 export default function Opening(){
+    // function TextWriter({ text, interval }) {
+    //     const [currentText, setCurrentText] = useState("");
+    //     const [index, setIndex] = useState(0);
+    //
+    //     useEffect(() => {
+    //         const timer = setInterval(() => {
+    //             if (index >= text.length) {
+    //                 clearInterval(timer);
+    //             } else {
+    //                 setCurrentText((prevText) => prevText + text[index]);
+    //                 setIndex((prevIndex) => prevIndex + 1);
+    //             }
+    //         }, interval);
+    //
+    //         return () => clearInterval(timer);
+    //     }, [index, interval, text]);
+    //
+    //     return <div>{currentText}</div>;
+    // }
+
+    // <TextWriter text="Hello, world!" interval={100} />
+
     return(
-        <div className="App">
-            <h1>Hi Mom!</h1>
-            <ul className="sidebar-content">
-                <li className="sidebar-item">
-                    <Link to="/library"><FaBookOpen size="2rem"/>
-                        <span style={{marginLeft : "1rem"}}>Library</span>
-                    </Link>
-                </li>
-            </ul>
+        <div className="opening">
+            <div className="sidebar-logo">
+                <Link to="/library"><h1>ReadBook</h1></Link>
+            </div>
         </div>
     )
 }
